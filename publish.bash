1cf407103edb299c574f4e6a96ce4314c2507415
Tom Kent <tom@teeks99.com>
2023-02-22 20:22:16 -0600

    Publish cleanup

--- publish.bash
+++ publish.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
-rsync -aP --delete --stats \
+sed 's/teeks99.github.io/teeks99.com/' _config.yml > _config_site_build.yml \
+&& docker run --rm \
+  --volume="$PWD:/srv/jekyll:Z" \
+  jekyll/jekyll \
+  jekyll build --destination _site_build --config _config_site_build.yml \
+&& rsync -aP --delete --stats \
   --exclude keep \
   --exclude tmp \
   --exclude laura \
-  _site/ lovejoy.teeks99.com:teeks99.com/
+  _site_build/ lovejoy.teeks99.com:teeks99.com/
